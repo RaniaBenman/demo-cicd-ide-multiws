7f1a718e7c53c5f0cb74ed7ace0b043cb0b07095
Rania Benman <rania.benman@databricks.com>
2022-06-01 02:20:31 +0200

    WIP - tests

--- tests/unit/sample_test.py
+++ tests/unit/sample_test.py
@@ -20,8 +20,9 @@ class SampleJobUnitTest(unittest.TestCase):
     def test_sample(self):
         # feel free to add new methods to this magic mock to mock some particular functionality
         self.job.dbutils = MagicMock()
-
-        df = self.spark.read.option("delimiter", ";").option("header", "true").csv("../tests/unit/data/turbines_sample.csv").createOrReplaceTempView("hive_metastore.default.turbines")
+        print("******")
+        print(os.path.join(os.path.dirname(__file__), '../tests/unit/data/turbines_sample.csv'))
+        df = self.spark.read.option("delimiter", ";").option("header", "true").csv(os.path.join(os.path.dirname(__file__), '../tests/unit/data/turbines_sample.csv')).createOrReplaceTempView("hive_metastore.default.turbines")
 
         self.job.launch()
 
