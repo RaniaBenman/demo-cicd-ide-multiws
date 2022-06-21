14caba19ad5a2856389806c94014548d9373ea65
Rania Benman <rania.benman@databricks.com>
2022-06-21 16:05:23 +0200

    Added dummy sql select

---
+++ notebooks/ptest.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC select * from hive_metastore.default.turbines_original 
+
+# COMMAND ----------
+
+# MAGIC 
+# MAGIC %sql
+# MAGIC show tables hive_metastore.default;--todo
+
+# COMMAND ----------
